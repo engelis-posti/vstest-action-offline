1315c0504cc39d8e95a8c6e0b7976dbe34b27c9c
Andris Engelis <andris.engelis@cgi.com>
2024-07-04 19:00:45 +0300

    one more path fix attempt

--- dist/index.js
+++ dist/index.js
@@ -1084,34 +1084,14 @@ function onceStrict (fn) {
 /***/ }),
 
 /***/ 81:
-/***/ (function(__unusedmodule, exports, __webpack_require__) {
+/***/ (function(__unusedmodule, exports) {
 
 "use strict";
 
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getVsTestPath = void 0;
-const path = __importStar(__webpack_require__(622));
 function getVsTestPath() {
-    return path.join("C:/Program\ Files/Microsoft\ Visual\ Studio/2022/Enterprise/", "Common7/IDE/CommonExtensions/Microsoft/TestWindow/vstest.console.exe");
+    return "C:/Program Files/Microsoft Visual Studio/2022/Enterprise/Common7/IDE/CommonExtensions/Microsoft/TestWindow/vstest.console.exe";
     // let vstestLocationMethod = core.getInput('vstestLocationMethod')
     // if(vstestLocationMethod && vstestLocationMethod.toUpperCase() === "LOCATION") {
     //   return core.getInput('vstestLocation')
